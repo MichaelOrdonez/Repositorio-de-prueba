721b87c20ac571a64eccb0b88e14016b614cf9db
MichaelOrdonez <mikael.oie@hotmail.com>
2025-03-25 23:03:07 -0500

    Prueba-Uno

---
+++ Sin_ttulo5.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 25 10:03:23 2025
+
+@author: lab
+"""
+
+lista=["R10","R2","R3","R4","S1","S2",
+       "S3","AP1","AP3","AP1","AP2","AP3","FW1"]
+for item in lista:
+    if "R" in item:
+        print(item,end=" ")
+    
